812c4732f4f4609af6bc30952de75408efa2d4f9
unknown <mohsen.shakeri.2012@gmail.com>
2025-07-22 20:57:13 +0330

    chore: create base modules and shared folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { JobsModule } from './jobs/jobs.module';
+import { ApplicationsModule } from './applications/applications.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, JobsModule, ApplicationsModule],
   controllers: [],
   providers: [],
 })
